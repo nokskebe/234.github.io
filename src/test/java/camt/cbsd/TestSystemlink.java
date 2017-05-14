b126a6caabda63d25a9f454f34d3bd18dc29e15b
DESKTOP-NDJUR90\WIN10 <Kittipan_panyausit@cmu.ac.th>
2017-05-14 23:48:02 +0700

    no message

--- src/test/java/camt/cbsd/TestSystemlink.java
+++ src/test/java/camt/cbsd/TestSystemlink.java
@@ -4,12 +4,17 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.thoughtworks.selenium.SeleneseTestBase.assertEquals;
 
 /**
  * Created by goes__000 on 5/14/2017.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestSystemlink {
     @Test
     public void Testlink() {
